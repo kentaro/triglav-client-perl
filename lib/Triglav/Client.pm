dbbc842e56a1834202835e8cabc03c205d2c03c2
Kentaro Kuribayashi <kentarok@gmail.com>
2012-11-05 21:53:00 +0900

    initial import

---
+++ lib/Triglav/Client.pm
@@ -0,0 +1,39 @@
+package Triglav::Client;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Triglav::Client - blah blah blah
+
+=head1 SYNOPSIS
+
+  use Triglav::Client;
+
+=head1 DESCRIPTION
+
+Triglav::Client is
+
+=head1 AUTHOR
+
+Kentaro Kuribayashi E<lt>kentarok@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kentaro Kuribayashi
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
